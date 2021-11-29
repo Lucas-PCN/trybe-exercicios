e2a320cc8ddf97b495da757fddb318e8d9856165
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2021-11-29 19:24:40 -0300

    adiciona alterações + exercicio 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_3.js
@@ -1,4 +1,4 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 let media = 0;
 
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio_4.js
@@ -1,8 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
+let media = 0;
 
 for(let index = 0; index < numbers.length; index += 1){
     sum = sum + numbers[index];
 }
 
-console.log(sum);
+media = sum/(numbers.length)
+
+if(media >20){
+    console.log("valor maior que 20")
+}
+else{
+    console.log("valor menor ou igual a 20")
+}
