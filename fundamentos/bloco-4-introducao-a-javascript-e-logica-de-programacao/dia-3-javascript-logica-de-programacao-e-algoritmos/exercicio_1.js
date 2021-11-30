0360899b7f393a2bfee263e075bfe789bb26d29f
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2021-11-30 19:38:25 -0300

    adiciona alterações

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio_1.js
@@ -0,0 +1,13 @@
+let n = 5;
+let asterisco = '*';
+let linha = '';
+
+for (let i = 1; i <= n; i += 1){
+    linha = linha + asterisco;
+};
+
+for(let i = 1; i <= n; i += 1){
+    console.log(linha);
+};
+
+    
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio_2.js
@@ -0,0 +1,8 @@
+let n = 5;
+let asterisco = '*';
+let linha = '';
+
+for (let i = 1; i <= n; i += 1){
+    linha = linha + asterisco;
+    console.log(linha);
+};
