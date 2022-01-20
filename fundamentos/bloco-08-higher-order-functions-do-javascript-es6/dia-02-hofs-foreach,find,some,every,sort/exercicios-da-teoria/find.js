3e14957db6890f5433f8dc104b7762812bec1352
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2022-01-20 18:44:30 -0300

    adiciona exercicios da teoria do dia 2 do bloco 8

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-hofs-foreach,find,some,every,sort/exercicios-da-teoria/find.js
@@ -0,0 +1,33 @@
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+    return numbers.find((number) => { return number % 3 === 0 && number % 5 === 0});
+};
+
+console.log(findDivisibleBy3And5());
+
+
+
+
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  return names.find((nome) => nome.length == 5);
+};
+
+console.log(findNameWithFiveLetters());
+
+
+
+const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  ]
+  
+  function findMusic(id) {
+    return musicas.find((music) => music.id === '31031685');
+  };
+  
+  console.log(findMusic('31031685'));
